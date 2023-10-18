9b79d0810671612517173d3fa35fadc60cf6425a
Kagamiin~ <kingchuxing.trills@aleeas.com>
2023-10-18 16:23:31 -0300

    Implement accurate envelope delay

--- esfm.h
+++ esfm.h
@@ -169,10 +169,11 @@ typedef struct _esfm_slot_internal
 	uint10 phase_out;
 	flag phase_reset;
 	flag *key_on;
+	flag key_on_gate;
 
 	uint2 eg_state;
 	flag eg_delay_run;
-	uint9 eg_delay_counter;
+	uint16 eg_delay_counter;
 
 } esfm_slot_internal;
 
